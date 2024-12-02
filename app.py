df2a3717c97f3067c2c445ba8883f18e147d81ae
net <net@PC1-06.igalvani.local>
2024-12-02 12:57:47 +0100

    aggiunto punto 3 della parte 2

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from flask import Flask, render_template
+#render_template: collegare file HTML.
+@app.route('/')
+def home():
+    return render_template('index.html')
 from flask import Flask
 
 #inizializza l'app Flask
