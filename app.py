1da137f16a03eb860f4fdc0cd43d39e1be01d12c
DomenicoPalumboMagri <palumbo.magri.d@iisgalvanimi.edu.it>
2024-12-09 11:36:25 +0000

    fase 2 finito

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
-from flask import Flask, render_template
-#render_template: collegare file HTML.
-@app.route('/')
-def home():
-    return render_template('index.html')
-from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 #inizializza l'app Flask
-app = Flask(__name__)
 
-#rotta principale
+lista_spesa = []
+
 @app.route('/')
 def home():
-    return "Per ora funziona tutto"
+    return render_template('index.html', lista = lista_spesa)
+
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    if elemento:
+        lista_spesa.append(elemento)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista_spesa):
+        lista_spesa.pop(indice)
+    return redirect(url_for('home'))
+
+@app.route('/svuota', methods=['POST'])
+def svuota_lista():
+    lista_spesa.clear()  # Svuota la lista
+    return redirect(url_for('home'))
 
 #avvio dell'app Flask
 if __name__ == '__main__':
